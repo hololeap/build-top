e682febbe559543fc77bec860a1710e4f28dca5b
hololeap <hololeap@protonmail.com>
2023-12-27 19:51:53 -0700

    Finish event loops
    * Switch from IORef to MVar
    * Finish eventLoop and scanLoop
    * Refactor Main.hs to utilize new loops
    
    Signed-off-by: hololeap <hololeap@protonmail.com>

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -4,34 +4,37 @@
 
 module Main where
 
+import Control.Concurrent.MVar
 import Control.Monad.IO.Class
 import Control.Monad.Reader
-import Control.Monad.Trans.Maybe
 import Data.Bifunctor
-import qualified Data.HashMap.Strict as M
-import Data.IORef
 import Reflex
 import Reflex.Host.Headless
 import qualified System.Linux.Inotify as Inotify
 import Text.Pretty.Simple
 
 import BuildTop.Debug
-import BuildTop.Filesystem
 import BuildTop.Loop
+import BuildTop.Paths
 import BuildTop.State
-import qualified BuildTop.Watcher as W
+import BuildTop.Types
 
 app :: MonadHeadlessApp t m => m (Event t ())
 app = mdo
-    _ <- runMaybeT $ do
-        i <- liftIO $ Inotify.init
-        (e, fireE) <- newTriggerEvent
-        flip runReaderT (i, fireE) $ do
-            Just (rw,wm) <- scanState Nothing (RealPath "/var/tmp/portage")
-            wmRef <- liftIO $ newIORef wm
-            _ <- eventLoop wmRef
-            liftIO $ print $ M.size $ W.rootWatcher_Children rw
-            performEvent $ liftIO . pPrintForceColor . first (printEvent rw) <$> e
+
+    i <- liftIO $ Inotify.init
+    (e, fireE) <- newTriggerEvent
+    _ <- flip runReaderT (i, fireE) $ do
+        let err = error
+                $ "portage temp dir not found? " ++ show portageTempDir
+        s@(rw,_) <- maybe err pure =<< scanState Nothing portageTempDir
+        wmRef <- liftIO $ newMVar s
+        let debugEvent :: MyEvent -> IO ()
+            debugEvent = pPrintForceColor . first (printEvent rw)
+        _ <- performEvent $ liftIO . debugEvent <$> e
+        ea <- eventLoop wmRef
+        sa <- scanLoop portageTempDir wmRef
+        pure (ea,sa)
     liftIO $ putStrLn "end of 'app'"
     pure never
 
--- src/BuildTop/Loop.hs
+++ src/BuildTop/Loop.hs
@@ -2,28 +2,74 @@
 
 module BuildTop.Loop where
 
+import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async
+import Control.Concurrent.MVar
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Reader
 import qualified Data.HashMap.Strict as M
-import Data.IORef
 import Data.These
 import qualified System.Linux.Inotify as Inotify
 
+-- import BuildTop.Paths
+import BuildTop.Filesystem (RealPath)
 import BuildTop.State
 import BuildTop.Types
 
+import Debug.Pretty.Simple
+
+-- | Infinitely wait for inotify events, firing the reflex @Event@ whenever
+--   one comes in.
 eventLoop :: (MonadIO m, MonadBuildTop m)
-    => IORef WatchMap
-    -> m (Async a)
-eventLoop wmRef = do
+    => MVar BuildTopState
+    -> m (Async ())
+eventLoop mvar = do
     i <- askInotify
     fireE <- askFireEvent
     liftIO $ async $ forever $ do
-        wm <- readIORef wmRef
         e@(Inotify.Event w _ _ _) <- Inotify.getEvent i
-        forM_ (M.lookup w wm) $ \d ->
-            withWatchMapData d $ \filt _ _ _ -> fireE $
-                case filt e of
-                    Just bte -> These e bte
-                    Nothing -> This e
+
+        s0@(_,wm0) <- takeMVar mvar
+
+        let err = error -- FIXME: This may not be a wise idea inside a thread
+                $ "could not find Watch " ++ show w ++ " in WatchMap"
+        d <- maybe err pure $ M.lookup w wm0
+
+        -- updateWatchState requires a MonadBuildTop (e.g. ReaderT)
+        s <- flip runReaderT (i,fireE) $ withWatchMapData d $ \filt _ _ _ ->
+            case filt e of
+                Nothing -> do
+                    liftIO $ fireE $ This e
+                    pure s0
+                Just bte -> do
+                    liftIO $ fireE $ These e bte
+                    -- We can run this if the filter returned a BuildTopState
+                    updateWatchState e bte s0
+
+        putMVar mvar s
+
+-- | Repeatedly scan the portage temp dir, firing events and updating state.
+--
+--   This repeats every 1s
+scanLoop :: (MonadIO m, MonadBuildTop m)
+    => RealPath
+    -> MVar BuildTopState
+    -> m (Async ())
+scanLoop path mvar = do
+    i <- askInotify
+    fireE <- askFireEvent
+    liftIO $ async $ forever $ do
+        pTraceMForceColor "scanLoop fired"
+
+        s0 <- takeMVar mvar
+
+        -- scanState requires a MonadBuildTop (e.g. ReaderT)
+        s <- flip runReaderT (i, fireE) $ do
+            let err = error -- FIXME: This may not be a wise idea inside a thread
+                    $ "portage temp dir not found? " ++ show path
+            maybe err pure =<< scanState (Just s0) path
+
+        putMVar mvar s
+
+        threadDelay 1000000
